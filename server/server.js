0da928b711bbdb97e59c5403cb3be55dc19d579a
Rohit-Pandit <rohitpandit.190425@gmail.com>
2025-11-09 09:40:02 +0530

    added frontend vercel link to cors

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(urlencoded({extended:true}));
 app.use(cookieParser())
 app.use(cors({
-    origin: [process.env.BASE_URL, 'http://localhost:4000', 'http://localhost:5173' ],
+    origin: [process.env.BASE_URL, 'http://localhost:4000', 'http://localhost:5173','https://authentication-system-phi-ten.vercel.app/' ],
     methods: ['GET','POST','PUT','DELETE'],
     allowedHeaders : ['Content-Type', 'Authorization'],
     credentials : true
